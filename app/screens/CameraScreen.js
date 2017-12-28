2ed15f2f37cdfb21e018b6d7ace5db5188e34c65
Orr T <orrt@Orrs-MacBook-Pro-2.local>
2017-12-28 13:58:09 -0800

    Added Camera

--- app/screens/CameraScreen.js
+++ app/screens/CameraScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, Image, View, Text, Button } from 'react-native';
+import { StyleSheet, TouchableOpacity, Image, View, Text, Button } from 'react-native';
 import { StackNavigator } from 'react-navigation'; // 1.0.0-beta.14
+import Camera from 'react-native-camera';
+
 
 const ImageHeader = props => (
   <View>
@@ -23,17 +25,57 @@ class CameraScreen extends Component {
   static navigationOptions = ({ navigation, screenProps }) => ({
     header: (props) => <ImageHeader {...props} />,
   });
+
+  onBarCodeRead(e) {
+    console.log(
+        "Barcode Found!",
+        "Type: " + e.type + "\nData: " + e.data
+    );
+  }
+  
+  takePicture() {
+    const options = {};
+    //options.location = ...
+    this.camera.capture({metadata: options})
+      .then((data) => console.log(data))
+      .catch(err => console.error(err));
+  }
   
   render() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Camera Screen</Text>
-        <Button
-          onPress={() => this.props.navigation.navigate('Details')}
-          title="Go to details"
-        />
+        <Camera
+          ref={(cam) => {
+            this.camera = cam;
+          }}
+	        onBarCodeRead={this.onBarCodeRead.bind(this)}
+          style={styles.preview}
+          aspect={Camera.constants.Aspect.fill}>
+          <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+        </Camera>
       </View>
     )}
 }
+
+const styles = StyleSheet.create({
+container: {
+  flex: 1,
+  flexDirection: 'row',
+},
+preview: {
+  flex: 1,
+  justifyContent: 'flex-end',
+  alignItems: 'center'
+},
+capture: {
+  flex: 0,
+  backgroundColor: '#fff',
+  borderRadius: 5,
+  color: '#000',
+  padding: 10,
+  margin: 40
+}
+});
 
 export default CameraScreen;
